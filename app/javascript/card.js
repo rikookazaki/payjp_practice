e4da694c64279a48035114e955c3201b8fdc1f5e
rikookazaki <riko.35.oka@gmail.com>
2024-10-13 17:33:29 +0900

    サーバーサイドにフォーム送信

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,6 +21,7 @@ const pay = () => {
       numberElement.clear();
       expiryElement.clear();
       cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
     e.preventDefault();
   });
