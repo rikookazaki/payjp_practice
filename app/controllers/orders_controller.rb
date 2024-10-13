fd42cabe5a8075547a077b2e20d7091653f198fe
rikookazaki <riko.35.oka@gmail.com>
2024-10-13 17:36:57 +0900

    ストロングパラメーター設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:price)
+    params.require(:order).permit(:price).merge(token: params[:token])
   end
 end
